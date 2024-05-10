83617e56589b39b6b114d0fdab983fbdf6deb442
noorirshad <noorirshad708@gmail.com>
2024-05-10 12:12:44 +0500

    create new API for get message

---
+++ AutoDeplyment/Controllers/AutoDeployController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoDeplyment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoDeployController : ControllerBase
+    {
+     
+        [HttpGet]
+        [Route("get-success")]
+        public IActionResult GetMessage()
+        {
+            return Ok("Success");
+        }
+    }
+}
